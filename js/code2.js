9d0c4482739f674a5b8342ce37ee2496f8e2a1fe
axelgodoy12 <84821493+axelgodoy12@users.noreply.github.com>
2022-03-10 08:10:55 -0300

    update js

--- js/code2.js
+++ js/code2.js
@@ -1,5 +1,21 @@
-const formu = document.getElementById("formi")
-const gastoBtn = document.getElementById("gasto")
+const formu = document.getElementById("formi");
+const formu2 = document.getElementById("formi");
+const gastoBtn = document.getElementById("gasto");
+const ingresoBtn = document.getElementById("ingreso");
+
+// formu.addEventListener('submit', (event) => {
+//     event.preventDefault();
+
+//     let transaccionFormData = new FormData(formu);
+//     let transaccionObj = convertirFormDataToObj(transaccionFormData);
+//     if (isValidTransaccionForm(transaccionObj)) {
+//         saveTransaccionObj(transaccionObj);
+//         insertRowInHistoryTableIngreso(transaccionObj);
+//         formu.reset();
+//     } else {
+//         // mostrar error
+//     }
+// })
 
 formu.addEventListener('submit', (event) => {
     event.preventDefault();
@@ -15,12 +31,12 @@ formu.addEventListener('submit', (event) => {
     }
 })
 
-gastoBtn.addEventListener('click', (event) => {
-    let transaccionFormData = new FormData(formu);
-    let transaccionObj = convertirFormDataToObj(transaccionFormData);
-    if (isValidTransaccionForm(transaccionObj)) {
-        saveTransaccionObj(transaccionObj);
-        insertRowInHistoryTableGasto(transaccionObj);
+formu2.addEventListener('submit', (event) => {
+    let transaccionFormDataGasto = new FormData(formu);
+    let transaccionObjGasto = convertirFormDataToObjGasto(transaccionFormDataGasto);
+    if (isValidTransaccionForm(transaccionObjGasto)) {
+        saveTransaccionObjGasto(transaccionObjGasto);
+        insertRowInHistoryTableGasto(transaccionObjGasto);
         formu.reset();
     } else {
         // mostrar error
@@ -32,25 +48,31 @@ document.addEventListener("DOMContentLoaded", function (event) {
     transaccionObjArray.forEach(
         function (transaccionArray) {
             insertRowInHistoryTableIngreso(transaccionArray)
-            insertRowInHistoryTableGasto(transaccionArray)
         }
     )
 })
 
-// document.addEventListener("DOMContentLoaded", function (event) {
-//     let transaccionObjArray = JSON.parse(localStorage.getItem("transaccionData"))
-//     transaccionObjArray.forEach(
-//         function (transaccionArray) {
-//             insertRowInHistoryTableGasto(transaccionArray)
-//         }
-//     )
-// })
+document.addEventListener("DOMContentLoaded", function (event) {
+    let transaccionObjArrayGasto = JSON.parse(localStorage.getItem("transaccionData"))
+    transaccionObjArrayGasto.forEach(
+        function (transaccionArrayGasto) {
+            insertRowInHistoryTableGasto(transaccionArrayGasto)
+        }
+    )
+})
 
 function getNewTransaccionId() {
     let lastTransaccionId = localStorage.getItem("lastTransaccionId") || "-1";
     let NewTransaccionId = JSON.parse(lastTransaccionId) + 1;
     localStorage.setItem("lastTransaccionId", JSON.stringify(NewTransaccionId))
     return NewTransaccionId;
+}
+
+function getNewTransaccionIdGasto() {
+    let lastTransaccionIdGasto = localStorage.getItem("lastTransaccionIdGasto") || "-1";
+    let getNewTransaccionIdGasto = JSON.parse(lastTransaccionIdGasto) + 1;
+    localStorage.setItem("lastTransaccionIdGasto", JSON.stringify(getNewTransaccionIdGasto))
+    return getNewTransaccionIdGasto;
 }
 
 function convertirFormDataToObj(transaccionFormData) {
@@ -61,6 +83,17 @@ function convertirFormDataToObj(transaccionFormData) {
         "concepto": concepto,
         "cantidad": cantidad,
         "transaccionId": transaccionId,
+    }
+}
+
+function convertirFormDataToObjGasto(transaccionFormDataGasto) {
+    let conceptoGasto = transaccionFormDataGasto.get("concepto");
+    let cantidadGasto = transaccionFormDataGasto.get("cantidad");
+    let transaccionIdGasto = getNewTransaccionIdGasto();
+    return {
+        "concepto": conceptoGasto,
+        "cantidad": cantidadGasto,
+        "transaccionId": transaccionIdGasto,
     }
 }
 
@@ -79,6 +112,22 @@ function isValidTransaccionForm(transaccionObj) {
     }
     return isValidForm;
 }
+
+// function isValidTransaccionFormGasto(transaccionObjGasto) {
+//     let = isValidForm = true;
+//     if (!transaccionObjGasto["concepto"]) {
+//         alert("Por favor escribe un concepto de ingreso o gasto");
+//         isValidForm = false;
+//     }
+//     if (!transaccionObjGasto["cantidad"]) {
+//         alert("Por favor escribe una cantidad");
+//         isValidForm = false;
+//     } else if (transaccionObjGasto["cantidad"] < 0) {
+//         alert("Por favor no puede ingresar numeros negativos");
+//         isValidForm = false;
+//     }
+//     return isValidForm;
+// }
 
 function insertRowInHistoryTableIngreso(transaccionObj) {
     let historyTableRef = document.getElementById("historyTable");
@@ -109,22 +158,22 @@ function insertRowInHistoryTableIngreso(transaccionObj) {
     })
 }
 
-function insertRowInHistoryTableGasto(transaccionObj) {
-    let historyTableRef = document.getElementById("historyTable");
+function insertRowInHistoryTableGasto(transaccionObjGasto) {
+    let historyTableRefGasto = document.getElementById("historyTable");
 
-    let newHistoryRowRef = historyTableRef.insertRow(-1);
+    let newHistoryRowRefGasto = historyTableRefGasto.insertRow(-1);
 
-    newHistoryRowRef.setAttribute("data-transaccion-Id", transaccionObj["transaccionId"]);
+    newHistoryRowRefGasto.setAttribute("data-transaccion-Id-gasto", transaccionObjGasto["transaccionId"]);
 
-    let newHistoryCellRef = newHistoryRowRef.insertCell(0);
-    newHistoryCellRef.textContent = transaccionObj["concepto"];
+    let newHistoryCellRefGasto = newHistoryRowRefGasto.insertCell(0);
+    newHistoryCellRefGasto.textContent = transaccionObjGasto["concepto"];
 
-    newHistoryCellRef = newHistoryRowRef.insertCell(1);
-    newHistoryCellRef.textContent = transaccionObj["cantidad"];
-    let historialGasto = newHistoryCellRef;
+    newHistoryCellRefGasto = newHistoryRowRefGasto.insertCell(1);
+    newHistoryCellRefGasto.textContent = transaccionObjGasto["cantidad"];
+    let historialGasto = newHistoryCellRefGasto;
     historialGasto.classList.add("historialGasto");
 
-    let newDeleteCell = newHistoryRowRef.insertCell(2);
+    let newDeleteCell = newHistoryRowRefGasto.insertCell(2);
     let deleteButton = document.createElement("button");
     deleteButton.classList.add("deleteButton");
     deleteButton.textContent = "X";
@@ -132,7 +181,7 @@ function insertRowInHistoryTableGasto(transaccionObj) {
 
     deleteButton.addEventListener("click", (event) => {
         let transaccionRow = event.target.parentNode.parentNode;
-        let transaccinoId2 = transaccionRow.getAttribute("data-transaccion-Id");
+        let transaccinoId2 = transaccionRow.getAttribute("data-transaccion-Id-gasto");
         transaccionRow.remove();
         DeleteTransaccionObj(transaccinoId2);
     })
@@ -143,6 +192,13 @@ function saveTransaccionObj(transaccionObj) {
     myTransaccionArray.push(transaccionObj);
     let TransaccionArrayJSON = JSON.stringify(myTransaccionArray);
     localStorage.setItem("transaccionData", TransaccionArrayJSON);
+}
+
+function saveTransaccionObjGasto(transaccionObjGasto) {
+    let myTransaccionArrayGasto = JSON.parse(localStorage.getItem("transaccionData")) || [];
+    myTransaccionArrayGasto.push(transaccionObjGasto);
+    let TransaccionArrayGastoJSON = JSON.stringify(myTransaccionArrayGasto);
+    localStorage.setItem("transaccionData", TransaccionArrayGastoJSON);
 }
 
 // Le paso como parametro el transaccionId de la transaccion
@@ -160,5 +216,23 @@ function DeleteTransaccionObj(transaccionId){
     let TransaccionArrayJSON = JSON.stringify(transaccionObjArray);
     // GUARDO MI ARRAY EN LOCALSTORAGE
     localStorage.setItem("transaccionData", TransaccionArrayJSON); 
+
+}
+
+// Le paso como parametro el transaccionId de la transaccion
+// que quiero eliminar
+function DeleteTransaccionObjGasto(transaccionIdGasto){
+    // Obtengo las transaccion de mi "base de datos"
+    // DESCONVIERTO DE JSON A OBJETO
+    let transaccionObjArrayGasto = JSON.parse(localStorage.getItem("transaccionData"))
+    // Busco el indice o posicion de la transaccion que 
+    // quiero eliminar
+    let transaccionIndexArrayGasto = transaccionObjArrayGasto.findIndex(element => element.transaccionId == transaccionId);
+    // Elimino la transaccion o el elemento de esa posicion
+    transaccionObjArrayGasto.splice(transaccionIndexArrayGasto, 1);
+    // convierto de objeto a JSON
+    let TransaccionArrayGastoJSON = JSON.stringify(transaccionObjArrayGasto);
+    // GUARDO MI ARRAY EN LOCALSTORAGE
+    localStorage.setItem("transaccionData", TransaccionArrayGastoJSON); 
 
 }
---
+++ js/code3.js
@@ -0,0 +1,139 @@
+const formWallet = document.getElementById("formWallet");
+const formConcept = document.getElementById("formConcept");
+const formCash = document.getElementById("formCash");
+const addUp = document.getElementById("addUp");
+const addDown = document.getElementById("addDown");
+const walletTable = document.getElementById("walletTable");
+const template = document.getElementById("template").content;
+const fragment = document.createDocumentFragment();
+const conteinerForm = document.getElementById("conteinerForm");
+let income = {};
+let output = {};
+// new Document();
+
+// document.body = formWallet
+document.addEventListener("DOMContentLoaded", () => {
+    if (localStorage.getItem('income')) {
+        income = JSON.parse(localStorage.getItem('income'));
+    }
+    addIncome();
+})
+// document.addEventListener("DOMContentLoaded", () => {
+//     if (localStorage.getItem('output')) {
+//         output = JSON.parse(localStorage.getItem('output'));
+//     }
+//     addTask();
+// })
+
+// formWallet.addEventListener('submit', (e) => {
+//   // console.log('click')
+//   // console.log(e.target)
+//   // console.log(e.target.classList.contains('btn-info'))
+//     e.preventDefault();
+//     if (e.target.classList.contains("addUp")) {
+//         setIncome(e);
+//         return
+//     }
+//     e.stopPropagation()
+// });
+// formWallet.addEventListener('submit', (e) => {
+//   // console.log('click')
+//   // console.log(e.target)
+//   // console.log(e.target.classList.contains('btn-info'))
+//     e.preventDefault();
+//     if (e.target.classList.contains("addDown")) {
+//         setOutput(e);
+//         return
+//     }
+//     e.stopPropagation()
+// });
+
+
+
+
+// formWallet.addEventListener('submit', e => {
+//     e.preventDefault();
+//     // FORMAS DE SELECCIONAR LOS VALORES
+//     // console.log(e.target[0].value);
+//     // console.log(e.target.querySelector("input").value);
+//     // console.log(input.value);
+//     setIncome(e);
+//     // setOutput(e);
+//     formWallet.reset();
+// });
+
+
+
+const setOutput = (e) => {
+    if (formConcept.value.trim() === "" && formCash.value.trim() === "") {
+        alert("Completa los campos");
+        return
+    }
+    const egress = {
+        id: Date.now(),
+        texto: formConcept.value,
+        money: formCash.value,
+        estado: false
+    }
+    output[egress.id] = egress;
+    formConcept.focus();
+    addOutput();
+    // e.stopPropagation()
+}
+
+const setIncome = (e) => {
+    if (formConcept.value.trim() === "" && formCash.value.trim() === "") {
+        alert("Completa los campos");
+        return
+    }
+    const entry = {
+        id: Date.now(),
+        texto: formConcept.value,
+        money: formCash.value,
+        estado: false
+    }
+    income[entry.id] = entry;
+    // formWallet.reset();
+    formConcept.focus();
+    addIncome();
+    // e.stopPropagation()
+};
+
+const addOutput = () => {
+    localStorage.setItem('output', JSON.stringify(output));
+    if (Object.values(output).length === 0) {
+        walletTable.innerHTML = ``;
+        return
+    }
+    walletTable.innerHTML = "";
+    Object.values(output).forEach(egress => {
+        const clone = template.cloneNode(true)
+        clone.querySelector('.walletConcept').textContent = egress.texto
+        clone.querySelector('.walletCash').textContent = egress.money
+        fragment.appendChild(clone)
+    });
+    walletTable.appendChild(fragment);
+}
+
+const addIncome = () => {
+    localStorage.setItem('income', JSON.stringify(income));
+    if (Object.values(income).length === 0) {
+        walletTable.innerHTML = ``;
+        return
+    }
+    walletTable.innerHTML = "";
+    Object.values(income).forEach(entry => {
+        const clone = template.cloneNode(true)
+        clone.querySelector('.walletConcept').textContent = entry.texto
+        clone.querySelector('.walletCash').textContent = entry.money
+        // if (task.estado) {
+        //     clone.querySelector('.taskItems').classList.add('taskItemsConteiner');
+        //     clone.querySelectorAll('.fa-solid')[0].classList.replace('fa-circle-check', 'fa-undo-alt');
+        //     clone.querySelector('p').style.textDecoration = 'line-through';
+        // }
+        // clone.querySelectorAll('.fa')[0].dataset.id = entry.id
+        // clone.querySelectorAll('.fa')[1].dataset.id = entry.id
+        fragment.appendChild(clone)
+    });
+    walletTable.appendChild(fragment);
+};
