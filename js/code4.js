9dfa00e4c61cad8e677b4e1d8aaafd4bf50a18e1
axelgodoy12 <84821493+axelgodoy12@users.noreply.github.com>
2022-03-11 12:37:48 -0300

    update js css

--- js/code4.js
+++ js/code4.js
@@ -5,8 +5,10 @@ const addUp = document.getElementById("addUp");
 const addDown = document.getElementById("addDown");
 const walletTable = document.getElementById("walletTable");
 const template = document.getElementById("template").content;
+const template2 = document.getElementById("template2").content;
 const fragment = document.createDocumentFragment();
 const conteinerForm = document.getElementById("conteinerForm");
+const walletRow = document.getElementById("walletRow");
 let income = {};
 let output = {};
 
@@ -20,22 +22,9 @@ document.addEventListener("DOMContentLoaded", () => {
     addIncome();
     addOutput();
 })
-// document.addEventListener("DOMContentLoaded", () => {
-//     if (localStorage.getItem('output')) {
-//         output = JSON.parse(localStorage.getItem('output'));
-//     }
-//     addOutput();
-// })
-// document.addEventListener("DOMContentLoaded", () => {
-//     if (localStorage.getItem('income')) {
-//         income = JSON.parse(localStorage.getItem('income'));
-//     }
-//     addIncome();
-//     if (localStorage.getItem('output')) {
-//         output = JSON.parse(localStorage.getItem('output'));
-//     }
-//     setOutput();
-// })
+walletRow.addEventListener('click', (e) => {
+    btnDelete(e);
+})
 conteinerForm.addEventListener('click', (e) => {
   // console.log('click')
   // console.log(e.target)
@@ -86,9 +75,11 @@ const addIncome = () => {
     }
     walletTable.innerHTML = "";
     Object.values(income).forEach(entry => {
-        const clone = template.cloneNode(true)
+        let clone = template.cloneNode(true)
         clone.querySelector('.walletConcept').textContent = entry.texto
-        clone.querySelector('.walletCash').textContent = entry.money
+        clone.querySelector('.walletCashIn').textContent = entry.money
+        clone.querySelectorAll('.wallet')[0].dataset.id = entry.id
+        clone.querySelectorAll('.wallet')[1].dataset.id = entry.id
         // if (task.estado) {
         //     clone.querySelector('.taskItems').classList.add('taskItemsConteiner');
         //     clone.querySelectorAll('.fa-solid')[0].classList.replace('fa-circle-check', 'fa-undo-alt');
@@ -127,10 +118,27 @@ const addOutput = () => {
     }
     walletTable.innerHTML = "";
     Object.values(output).forEach(egress => {
-        const clone = template.cloneNode(true)
+        let clone = template2.cloneNode(true)
         clone.querySelector('.walletConcept').textContent = egress.texto
-        clone.querySelector('.walletCash').textContent = egress.money
+        clone.querySelector('.walletCashOut').textContent = egress.money
+        clone.querySelectorAll('.wallet')[0].dataset.id = egress.id
+        clone.querySelectorAll('.wallet')[1].dataset.id = egress.id
         fragment.appendChild(clone)
     });
     walletTable.appendChild(fragment);
+}
+
+const btnDelete = (e) => {
+    // console.log(e.target.classList.contains('fa-circle-check'));
+    if (e.target.classList.contains('deleteButton')) {
+        delete income[e.target.dataset.id];
+        addIncome();
+        // console.log(tareas);
+    }
+    if (e.target.classList.contains('deleteButton')) {
+        delete output[e.target.dataset.id];
+        addOutput();
+        // console.log(tareas);
+    }
+    e.stopPropagation()
 }
